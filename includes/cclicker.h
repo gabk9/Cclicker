decbfdf19c6d176d4804bb426f322f92c39fd36e
Gabriel Oiveira Miranda <giane.ga2010@gmail.com>
2026-08-02 22:43:22 -0300

    - removed 'cclicker_destroy()' prototype from cclicker.h

--- includes/cclicker.h
+++ includes/cclicker.h
@@ -10,7 +10,6 @@ typedef struct Cclicker Cclicker;
 
 Cclicker *cclicker_create(void);
 void cclicker_start(Cclicker *cc);
-void cclicker_destroy(Cclicker *cc);
 void cclicker_set_cps(Cclicker *cc, double cps);
 void cclicker_set_delay_s(Cclicker *cc, double delay_s);
 void cclicker_set_button(Cclicker *cc, m_button button);
